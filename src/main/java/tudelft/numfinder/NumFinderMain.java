05ace7f94d52666850f6afab3fb2bda4cd0ba11a
Yasser Zadeh <yassermzh.germany@gmail.com>
2021-02-20 15:43:43 +0100

    Fix Num finder

--- src/main/java/tudelft/numfinder/NumFinderMain.java
+++ src/main/java/tudelft/numfinder/NumFinderMain.java
@@ -9,7 +9,9 @@ public class NumFinderMain {
         // nf.find(new int[] {4, 25, 7, 9});
 
         // this crashes
-        nf.find(new int[] {4, 3, 2, 1});
+//        nf.find(new int[] {4, 3, 2, 1});
+
+        nf.find(null);
 
         System.out.println(nf.getLargest());
         System.out.println(nf.getSmallest());
